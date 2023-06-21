70c5251545ab77baa64848eff5924aaf7100942d
Dioxide6345 <112908448+Dioxide6345@users.noreply.github.com>
2023-06-21 10:18:02 +1000

    Update Bench1.py

--- Bench1.py
+++ Bench1.py
@@ -1,1 +1,14 @@
+import datetime
 
+def dayOfTheWeek(day, month, year):
+    date = datetime.date(year, month, day)
+    weekday = date.weekday()
+    day_dict = { 0 : "Monday", 1 : "Tuesday", 2 : "Wednesday", 3 : "Thursday", 4 : "Friday", 5 : "Sturday", 6 : "Sunday"}
+
+    return day_dict[weekday]
+    
+year = int(input("Year: "))     
+month = int(input("Month: "))       
+day = int(input("Day: "))
+
+dayOfTheWeek(day, month, year)
