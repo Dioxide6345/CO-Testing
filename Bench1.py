5b20d2bea1144aad4f9730db5b26527b1dab1a35
Dioxide6345 <112908448+Dioxide6345@users.noreply.github.com>
2023-06-21 10:15:54 +1000

    Update Bench1.py

--- Bench1.py
+++ Bench1.py
@@ -1,13 +1,1 @@
-import requests
-import os
-import json
-from dotenv import load_dotenv
-load_dotenv()
-api_key=os.getenv("api_key")
-playerINT = 44067861
 
-playerID = '44067861'
-base_url = 'https://www.opendota.com/players/{playerID}/heroes?patch=50&having=5'
-endpoint = f"{base_url}?api_key={api_key}"
-r = requests.get(endpoint)
-print(r.json())
